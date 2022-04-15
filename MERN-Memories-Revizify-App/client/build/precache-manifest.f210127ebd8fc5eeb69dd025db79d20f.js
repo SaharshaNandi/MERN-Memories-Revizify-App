80e19eaeeb3b4d95e916a90f5cedb789b987d185
Saharsha30 <60267353+SaharshaNandi@users.noreply.github.com>
2022-04-15 23:11:18 +0530

    Add files via upload

---
+++ MERN-Memories-Revizify-App/client/build/precache-manifest.f210127ebd8fc5eeb69dd025db79d20f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8081e845f4811518f13db8d3aa5047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f08b1655eacf06c84c30",
+    "url": "/static/css/main.646a6327.chunk.css"
+  },
+  {
+    "revision": "acdbda63085ca11a42a5",
+    "url": "/static/js/2.c965c6c6.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.c965c6c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08b1655eacf06c84c30",
+    "url": "/static/js/main.8131a112.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/logo.dc8bbfe3.png"
+  },
+  {
+    "revision": "7108ac90f9b2c6b9980c510176517744",
+    "url": "/static/media/sweetMemories.7108ac90.png"
+  }
+]);
